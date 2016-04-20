af26600b17d02eb54891430c48c415a87835fba6
zhendong <myyizhendong@gmail.com>
2016-04-20 09:57:57 +0800

    model

---
+++ src/main/java/com/dong/cbu/model/ActorsInMovie.java
@@ -0,0 +1,8 @@
+package com.dong.cbu.model;
+
+/**
+ * Created by zhendong on 2016/4/20.
+ * email:myyizhendong@gmail.com
+ */
+public class ActorsInMovie {
+}
---
+++ src/main/java/com/dong/cbu/model/Movie.java
@@ -0,0 +1,80 @@
+package com.dong.cbu.model;
+
+/**
+ * Created by zhendong on 2016/4/20.
+ * email:myyizhendong@gmail.com
+ */
+public class Movie {
+    private int id;
+    private String name;
+    private String director;
+    private String actor;
+    private int count;
+    private String type;
+    private int status;
+    private String introduction;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getActor() {
+        return actor;
+    }
+
+    public void setActor(String actor) {
+        this.actor = actor;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+}
